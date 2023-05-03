6c28b86ae4ad6834412ca140e25dbc71dedade09
Dan Hult <Dan.hult@hotmail.com>
2023-05-03 12:55:13 +0200

    First commit

---
+++ GrupparbeteMVC/Controllers/TicketsController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Grupparbete.Models;
+using GrupparbeteMVC;
+using Microsoft.EntityFrameworkCore;
+using GrupparbeteMVC.Models;
+
+namespace Grupparbete.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly ApplicationContext Context;
+        public TicketsController(ApplicationContext context)
+        {
+            Context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Ticket> MyTickets = Context.Tickets.ToList();
+            return View(MyTickets);
+        }
+        public string MyTicket()
+        {
+            return "de här problemen registrerades idag.";
+        }
+        public IActionResult Details(int Id)
+        {
+            //System.Diagnostics.Debug.Writeline("Id: " ´Id);
+            Ticket Ticket = Context.Tickets.FirstOrDefault(x => x.Id == Id);
+            return View(Ticket);
+
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string Title, string Description, string Creator)
+        {
+            Ticket NewTicket = new Ticket();
+            NewTicket.Title = Title;
+            NewTicket.Description = Description;
+            NewTicket.Creator = Creator;
+            Context.Tickets.Add(NewTicket);
+            Context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(int Id)
+        {
+            Ticket Ticket = Context.Tickets.FirstOrDefault(x => x.Id == Id);
+            return View(Ticket);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int Id, string Title, string Description, string Creator)
+        {
+            Ticket NewTicket = Context.Tickets.FirstOrDefault(x => x.Id == Id);
+            NewTicket.Title = Title;
+            NewTicket.Description = Description;
+            NewTicket.Creator = Creator;
+            Context.Tickets.Update(NewTicket);
+            Context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+        public IActionResult Delete(int Id)
+        {
+            Ticket Ticket = Context.Tickets.FirstOrDefault(x => x.Id == Id);
+            return View(Ticket);
+        }
+        public IActionResult DeleteConfirm(int Id)
+        {
+            Ticket Ticket = Context.Tickets.FirstOrDefault(x => x.Id == Id);
+            Context.Remove(Ticket);
+            Context.SaveChanges(true);
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+
+        public IActionResult Reply(int id)
+        {
+            var ticket = Context.Tickets.FirstOrDefault(t => t.Id == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        [HttpPost]
+        public IActionResult Reply(int id, string reply)
+        {
+            var ticket = Context.Tickets.FirstOrDefault(t => t.Id == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            ticket.Reply = reply;
+
+            Context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+    }
+}
---
+++ GrupparbeteMVC/Models/Ticket.cs
@@ -0,0 +1,14 @@
+﻿namespace Grupparbete.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Creator { get; set; }
+        public string? Reply { get; set; }
+
+        
+    }
+
+}
